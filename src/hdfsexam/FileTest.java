d2b975ba300ecd57d628ded3f45a11073f471eb6
Chanwoo <overelectric@gmail.com>
2020-04-29 17:46:19 +0900

    update hadoopjava

--- src/hdfsexam/FileTest.java
+++ src/hdfsexam/FileTest.java
@@ -3,33 +3,39 @@ package hdfsexam;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.rmi.Remote;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 
 public class FileTest {
-	
-	private static final String srcDir = "/edudata/" ;
+
+	private static final String srcDir = "/edudata/";
 
 	public static void main(String[] args) throws Exception {
 		String fileName = "president_moon.txt";
 		Path path = new Path(srcDir + fileName);
 		Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", "hdfs://192.168.111.120:9000");
+		conf.set("fs.defaultFS", "hdfs://192.168.111.120:9000");
 		FileSystem fs = FileSystem.get(URI.create(srcDir + fileName), conf);
-		
-		if(fs.exists(path)) { 
-			BufferedReader br =new BufferedReader(new InputStreamReader(fs.open(path)));
-			
-			while(br.ready()){ 	  	
+
+		if (fs.exists(path)) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(path)));
+
+			// 당연히 쓰면 안되는 형식이다. 왜나면 커질수록 오래 걸리는데
+			// 하둡은 행 단위로 읽으면 엄청 걸린다.
+			while (br.ready()) {
 				String line = br.readLine();
 				System.out.println(line);
-	        }
-			
+			}
+
+			// 파일 스테이터스 객체를 리턴 받아서 저장한다.
 			FileStatus fStatus = fs.getFileStatus(path);
-			if(fStatus.isFile()) {
+			if (fStatus.isFile()) {
 				System.out.println("");
 				System.out.println("===========================================");
 				System.out.println("File Block Size : " + fStatus.getBlockSize());
@@ -38,6 +44,16 @@ public class FileTest {
 				System.out.println("File Length     : " + fStatus.getLen());
 			} else {
 				System.out.println("파일이 아닙니다.");
+			}
+			// "/edudata/"에 대핸 패스를 생성한다.
+			path = new Path(srcDir);
+			// RemoteIterator<LocatedFileStatus>에 들어가 있는 데이터를 recursive해서 가져와라
+			// 만약 next가 가능한 동안 계쏙 가져와라.
+			RemoteIterator<LocatedFileStatus> list = fs.listFiles(path, false);
+			while (list.hasNext()) {
+				LocatedFileStatus is = list.next();
+				Path p = is.getPath();
+				System.out.println(p.getName());
 			}
 		} else {
 			System.out.println("파일을 찾을 수 없습니다.");
--- src/hdfsexam/FileWrite.java
+++ src/hdfsexam/FileWrite.java
@@ -13,24 +13,32 @@ import org.apache.hadoop.fs.Path;
 public class FileWrite {
 	public static void main(String[] args) {
 		try {
+			// 이 2줄운 하둡 환경 설정 같은 것이다.
+			// 즉, 하둡이 꺼져 있다면 여기에서 에러가 뜬다.
 			Configuration conf = new Configuration();
 			conf.set("fs.defaultFS", "hdfs://192.168.111.120:9000");
+			
 			FileSystem hdfs = FileSystem.get(conf);
-			String fname = "product_click.log";
-			File f = new File("c:/unico/" + fname);
+			String fname = "dessert-menu.csv";
+			File f = new File("C:/testcontent/" + fname);
 			if (!f.exists()) {
 				System.out.println("파일이 없음!!");
 				return;
 			}
 			Path path = new Path("/edudata/" + fname);
+			// 만약 이미 존재하고 있다면 삭제한다.
 			if (hdfs.exists(path)) {
 				hdfs.delete(path, true);
 			}
+			// 파일의 길이 읽어옴
 			long size = f.length();
+			// 이 객체를 생성한 이유는 file객체의 크기를 알아내기 위해서
 			FileReader fr = new FileReader(f);
 			char[] content = new char[(int) size];
 			fr.read(content);
+			// outStream 객체를을 새로 생성하겠다. 
 			FSDataOutputStream outStream = hdfs.create(path);
+			// bufferedwrite 객체로 집어 넣는다.
 			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
 			writer.write(content);
 			writer.close();
