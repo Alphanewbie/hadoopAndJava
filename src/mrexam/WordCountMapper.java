0c97b51f0b17d1feeb6a7565b50dd5e41bc045da
Chanwoo <overelectric@gmail.com>
2020-05-02 15:27:35 +0900

    Add WordCount

---
+++ src/mrexam/WordCountMapper.java
@@ -0,0 +1,30 @@
+package mrexam;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+// 매퍼라는 클래스를 상속하고 있다. 제너릭스로 4가지 타입 상속
+// 1,2 는 받는 타입 3,4는 리턴하는 타입
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+	private final static IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+
+	// 매개변수  : 키 , 값, context ->  servletcontext 처럼 이건 하둡의 맵 리듀스 프레임워크를 가르킨다. 
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		// 받은 것을 토큰으로 쪼갠다.
+		StringTokenizer itr = new StringTokenizer(value.toString());
+		while (itr.hasMoreTokens()) {
+			// 하둡의 분산환경에 특화 되어 있다ㅓ.
+			// 객체를 또 생성 안하고 그냥 갖다 쓴다.
+			word.set(itr.nextToken());
+			// 카운트를 1 증가 시킨다.
+			context.write(word, one);
+		}
+	}
+}
